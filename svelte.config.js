7f01fdbd2d48e8213f6b28a6766630aff76fb9c3
H3INM@WIT <heinmin2maw.it@gmail.com>
2024-06-12 23:10:32 +0700

    fix: svelte configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null
-    }),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/HM3IT.github.io' : ''
-    },
-    prerender: {
-      default: true
-    }
-  }
+	kit: {
+		adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+			fallback: '404.html'
+		}),
+		paths: {
+            base: process.env.NODE_ENV === 'production' ? '/HM3IT.github.io' : ''
+			
+		}
+	}
 };
 
 export default config;
