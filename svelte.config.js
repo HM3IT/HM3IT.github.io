a06f2c98f28fccaccd7b12cac7607658c126c3f0
H3INM@WIT <heinmin2maw.it@gmail.com>
2024-06-12 23:21:02 +0700

    fix: base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,21 @@
-import adapter from '@sveltejs/adapter-static';
+ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-			fallback: '404.html'
-		}),
-		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/HM3IT.github.io' : ''
-			
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html',
+  
+    }),
+ 
+ 
+ 
+  }
 };
 
 export default config;
